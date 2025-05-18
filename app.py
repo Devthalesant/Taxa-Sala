ed150941ac32f909c35fe6058e372ea73c8ad732
Thales <tha.santoro2000@gmail.com>
2025-05-18 20:17:25 -0300

    colocando segunda leva de perguntas

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Imersão 360- Taxa Sala")
+st.title("Imersão 360 - Taxa Sala")
 
 st.subheader("A seguir, faremos algumas perguntas para definir a Taxa Sala da sua clínica.")
 
@@ -15,5 +15,19 @@ total_despesas  = aluguel + funcionarios + demais
 st.markdown(
     "<h2 style='font-size:48px;'>Total de Despesas: R$ {valor}</h2>".format(
         valor=f"{total_despesas:,.2f}".replace(',', 'X').replace('.', ',').replace('X', '.')
+    ),
+    unsafe_allow_html=True)
+
+dias_uteis = st.number_input("Quantos dias uteis funciona?")
+horas_dia = st.number_input("Quantas horas por dia?")
+salas = st.number_input("Quantas salas de procedimento?")
+
+base_calculo = dias_uteis/horas_dia/salas
+
+taxa_sala = total_despesas/base_calculo
+
+st.markdown(
+    "<h2 style='font-size:48px;'>Taxa Sala: R$ {valor}</h2>".format(
+        valor=f"{taxa_sala:,.2f}".replace(',', 'X').replace('.', ',').replace('X', '.')
     ),
     unsafe_allow_html=True)
