282f35d322231da931b4e695d1eefc5896abde8f
Thales <tha.santoro2000@gmail.com>
2025-05-18 20:04:46 -0300

    colocando as primeiras perguntas

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ st.title("Imersão 360 Estética - Taxa Sala")
 st.subheader("A seguir, faremos algumas perguntas para definir a Taxa Sala da sua clínica.")
 
 
-nome = st.text_input("Digite seu nome")
-idade = st.number_input("Digite sua idade", min_value=0, max_value=120, step=1)
+#nome = st.text_input("Digite seu nome")
+aluguel = st.number_input("Qual o valor do seu aluguel?")
+funcionarios = st.number_input("Qual o gasto total com funcionarios?")
+demais = st.number_input("Qual o valor total com demais gastos?")
 
-st.write(f"Nome : {nome}")
+total_despesas  = aluguel + funcionarios + demais
+
+st.write(f"Total de Despeas: R$ {total_despesas:,.2f}".replace(',', 'X').replace('.', ',').replace('X', '.'))
+
