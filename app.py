042bb128dbcef2b4d6700a38cea73cc8de4af0d3
Thales <tha.santoro2000@gmail.com>
2025-05-20 13:13:08 -0300

    Paginamento

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ elif st.session_state['page'] == 3:
     consumivel = st.number_input("Qual o gasto com consumivel? (R$)", min_value=0, step=1)
     aliquota = st.number_input("Qual a aliquota de imposto? (%)", min_value=0, step=0.01) / 100
     cartao = st.number_input("Qual a taxa de cartão de crédito? (%)", min_value=0.01, step=0.01) / 100
-    comissao = st.number_input("Qual o comissionamento de venda? (%)", min_value=0.01 step=0.01) / 100
+    comissao = st.number_input("Qual o comissionamento de venda? (%)", min_value=0.01, step=0.01) / 100
     mod = st.number_input("Qual valor pago ao profissinal pela execucao? (R$)", min_value=0.01, step=1)
